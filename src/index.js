0ee017c6550a729b673c05b411b69d95db8e15c2
hsoubecommerce <hsoubecommerce@gmail.com>
2021-05-27 00:45:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -6,33 +6,49 @@ import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/js/all.min';
 import 'bootstrap';
+import 'bootstrap/js/dist/dropdown';
 import 'webpack-jquery-ui';
 import 'webpack-jquery-ui/css';
 import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';z
-$(window).scroll(function(){
-    if($(this).scrollTop()>=500){
-       $("#navBar").addClass("noTransparrent");
-     }else{
-       $("#navBar").removeClass("noTransparrent");
-     }
- });
- $(document).ready(function() {
-  var slider = $("#slideshow");
-  var slider_nav = $("#slideshow-nav");
-  slider_nav.find("a[href=#slide1]").addClass("active");
 
-  slider_nav.localScroll({
-    target:'#carouselExampleIndicators',
-    axis: 'x'
+$(document).ready(function(ready) {
+  $(".first-list-element").addClass("active");
+  $(".nav-item").click(function () {
+      $(".nav-item").removeClass("active");
+      $(this).addClass("active");
+  });
+  $("#toggler").click(function(event) {
+      $("#copyright").text(" 2020 أكاديمية حسوب" + new Date().getFullYear());
+      $('#wrap').toggleClass('toggled');
+
+      //إضافة طبقة سوداء
+      var right = $('.sidebar').css("right"); //أعطينا هذه الخاصية للقائمة الجانبية لكي تُصبح على اليمين
+      if (right == '0px') //القائمة الجانبية بحالة إظهار وبالتالي يجب إغلاقها
+      {
+          $('.sidebar').css({ 'right': '-17rem' });
+          $('.layer').fadeOut(); /*أخفي الطبقة السوداء*/
+      }
+      else { //القائمة الجانبية بحالة إغلاق وبالتالي يجب إظهارها
+          $('.sidebar').css({ 'right': '0' });
+          $('.layer').fadeIn(); /*أظهر الطبقة السوداء*/
+      }
+  });
+  new Date().getFullYear()
+  {
+      document.getElementById("year").innerHTML = new Date().getFullYear();
+      }
+
+     
+
+  //إخفاء القائمة الجانبية والطبقة السوداء عند الضغط على الطبقة السوداء
+  $('.layer').click(function () {
+      $('.sidebar').css({ 'right': '-17rem' });
+      $('.layer').fadeOut();
   });
 
-  slider_nav.find("a").click(function(){
-    slider_nav.find("a").removeClass("active");
-    $(this).addClass("active");
+  //إخفاء وإظهار حقل البحث عند الضغط على أيقونة البحث التي تظهر في الشاشات المتوسطة والصغيرة
+  $(".search-icon").click(function () {
+      $(".search-input").slideToggle("slow"); 
   });
-  var myCollapsible = document.getElementById('myCollapsible')
-myCollapsible.addEventListener('hidden.bs.collapse', function () {
-  // do something...
-})
 
 });
